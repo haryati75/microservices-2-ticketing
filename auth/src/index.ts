ec190b28216e7990787d603c2ff657394a81a852
Haryati H <haryati75@gmail.com>
2026-01-11 17:11:56 +0800

    feat: implement custom error handling and validation for user signup

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,11 +1,19 @@
 import express from 'express';
 import router from './routes/index.js';
+import { errorHandler } from './middlewares/error-handler.js';
+import { NotFoundError } from './errors/not-found-error.js';
 
 const app = express();
 const port = process.env.PORT ?? 3000;
 
 app.use(express.json());
 app.use('/api/users', router);
+
+// Catch-all middleware for unmatched routes
+app.use((req, res, next) => {
+  next(new NotFoundError());
+});
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`Listening on http://localhost:${String(port)}!`);
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,29 +1,36 @@
 import express, { Request, Response } from 'express';
 import { body, validationResult } from 'express-validator';
+import { RequestValidationError } from '../errors/request-validation-error.js';
+import { DatabaseConnectionError } from '../errors/database-connection-error.js';
 
 const router = express.Router();
 
 router.post(
   '/',
-  body('email').notEmpty().isEmail().withMessage('🦤Email must be valid!'),
-  body('password')
-    .notEmpty()
-    .trim()
-    .isLength({ min: 4, max: 20 })
-    .withMessage('🐣 Password must be between 4 and 20 characters'),
-  (req: Request, res: Response) => {
+  [
+    body('email').isEmail().withMessage('🦤 Email must be valid!'),
+    body('password')
+      .trim()
+      .isLength({ min: 4, max: 20 })
+      .withMessage('🐣 Password must be between 4 and 20 characters'),
+  ],
+  // eslint-disable-next-line @typescript-eslint/require-await
+  async (req: Request, res: Response) => {
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
-      console.log('🐓 Validation errors!');
-      return res.status(400).send({ errors: errors.array() });
+      throw new RequestValidationError(errors.array());
     }
 
     const { email, password } = req.body;
 
     // TODO: Add logic to check if user already exists
 
-    console.log(`🐦 Creating user with email: ${String(email)} and password: ${String(password)}`);
+    console.log(
+      `🐦 Creating user with email: ${String(email)} and password: ${String(password)}`,
+    );
+
+    throw new DatabaseConnectionError();
 
     // TODO: new User({ email, password }).save();
     // TODO: Add logic to check db save errors
